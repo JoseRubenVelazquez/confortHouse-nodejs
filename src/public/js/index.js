19b3d6150bae3b1f1cef728767e92289a0a38178
031186 <joseruben.velazquez@hotmail.com>
2020-09-28 17:09:15 -0500

    Primer commit a proyecto

---
+++ src/public/js/index.js
@@ -0,0 +1,9 @@
+$(".slider-promotions")
+.not(".slick-intialized")
+.slick({
+    prevArrow:".site-slider .slider-btn .prev",
+    nextArrow:".site-slider .slider-btn .next",
+    slidesToShow:5,
+    slideToScroll:1,
+    autoplaySpeed:3000
+});
